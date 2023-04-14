3bdc75b309333a16a49c72fa7152b32d7c5a4509
Shree <109030986+Shree-77@users.noreply.github.com>
2023-04-14 11:14:47 +0530

    UI designed

--- stateful/lib/main.dart
+++ stateful/lib/main.dart
@@ -1,59 +1,108 @@
+
 import 'package:flutter/material.dart';
 
-void main()=> runApp(MaterialApp(home: add(
+void main() => runApp(MaterialApp(home: Calculator(),
+  debugShowCheckedModeBanner: false,
+));
 
-)
-)
-);
-
-class add extends StatefulWidget {
-  const add({Key? key}) : super(key: key);
+class Calculator extends StatefulWidget {
+  const Calculator({Key? key}) : super(key: key);
   @override
-  State<add> createState() => _addState();
+  State<Calculator> createState() => _CalculatorState();
 }
 
-class _addState extends State<add> {
-  int age =0;
+class _CalculatorState extends State<Calculator> {
 
+  Widget numberButton(String btnText, Color btnColor , Color txtColor){
+    return ElevatedButton(onPressed: () {}, child: Text(
+      btnText , style: TextStyle(fontSize: 25,color: txtColor),
+    ),
+      style: ElevatedButton.styleFrom(
+        fixedSize: Size(70,70),
+        shape: CircleBorder(),
+        primary: btnColor,
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[200],
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
+        backgroundColor: Colors.orange[800],
         centerTitle: true,
-        title: Text('StatefulWidget' ,),
+        title: Text('Calculator'),
       ),
-
-       floatingActionButton :  FloatingActionButton(onPressed: () {
-        setState(() {
-          age+=1;
-        });
-        },child:
-        Text('+'),)
-      ,
-
-      body: Padding(
-        padding:EdgeInsets.all(20),
-
+      body:Padding(
+        padding: EdgeInsets.symmetric(horizontal: 5),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            Text('Name' , style: TextStyle(fontWeight: FontWeight.bold
-                ,fontSize:30.0 ),),
-            SizedBox(height: 10),
-            Text('Luffy' , style: TextStyle(fontWeight: FontWeight.bold
-                ,fontSize:30.0 ),),
-            SizedBox(height: 10),
-            Text('Age' , style: TextStyle(fontWeight: FontWeight.bold
-                ,fontSize:30.0 ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+                children:[
+                Padding(padding: EdgeInsets.all(10)
+                , child :Text(
+                      "0" , textAlign: TextAlign.left,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 80,
+                      ),
+                    )
+                ),
+              ]
             ),
-            SizedBox(height: 10.0),
-            Text('$age' , style: TextStyle(fontWeight: FontWeight.bold
-                ,fontSize:30.0 ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                numberButton("AC",Colors.grey[600]!, Colors.black),
+                numberButton("[x]",Colors.grey[600]!, Colors.black),
+                numberButton("%",Colors.grey[600]!, Colors.black),
+                numberButton("/",Colors.orange[600]!, Colors.black),
+              ],
             ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  numberButton("7",Colors.grey[600]!, Colors.black),
+                  numberButton("8",Colors.grey[600]!, Colors.black),
+                  numberButton("9",Colors.grey[600]!, Colors.black),
+                  numberButton("X",Colors.orange[600]!, Colors.black),
+                ],
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                numberButton("4",Colors.grey[600]!, Colors.black),
+                numberButton("5",Colors.grey[600]!, Colors.black),
+                numberButton("6",Colors.grey[600]!, Colors.black),
+                numberButton("-",Colors.orange[600]!, Colors.black),
+              ],
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                numberButton("1",Colors.grey[600]!, Colors.black),
+                numberButton("2",Colors.grey[600]!, Colors.black),
+                numberButton("3",Colors.grey[600]!, Colors.black),
+                numberButton("+",Colors.orange[600]!, Colors.black),
+              ],
+            ),
+            SizedBox(height: 10),
 
-
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                numberButton("0",Colors.grey[600]!, Colors.black),
+                numberButton("00",Colors.grey[600]!, Colors.black),
+                numberButton(".",Colors.grey[600]!, Colors.black),
+                numberButton("=",Colors.orange[600]!, Colors.black),
+              ],
+            ),
+            SizedBox(height: 20)
           ],
         ),
       ),
